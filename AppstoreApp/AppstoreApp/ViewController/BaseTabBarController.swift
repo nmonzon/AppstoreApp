4ab1eba2df5c3990d2c6abd667db3aae7ef1b0ad
Nicolas Monzon <nicolasmonzon@appstractweb.com>
2022-03-13 10:35:04 -0300

    refactoring navControllers from UITabBarController

---
+++ AppstoreApp/AppstoreApp/ViewController/BaseTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTabBarController.swift
+//  AppstoreApp
+//
+//  Created by Nicolas Monzon on 13/03/2022.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "today_icon"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "apps"),
+            createNavController(viewController: AppsSearchCollectionViewController(), title: "Search", imageName: "search")
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UINavigationController{
+        
+        viewController.view.backgroundColor = UIColor.white
+        viewController.navigationItem.title = title
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        navController.navigationBar.prefersLargeTitles = true
+        
+        return navController
+    }
+}
